b66c335b0c86d3373c3eb5e732964374e3ce728e
Level1Rookie <J0423@tw.intranet>
2019-10-04 16:37:25 +0800

    debug traverse_param_tree debug sieve_url_with_param

--- src/crawler104Core.py
+++ src/crawler104Core.py
@@ -2,11 +2,13 @@ from bs4 import BeautifulSoup
 import re
 import requests
 from requests.adapters import HTTPAdapter
-import time
-import pandas
-import os
+
+#xml related imports
 import settings
+import os
 from xml.etree import ElementTree as xml_etree_ElementTree
+
+import pandas
 
 class Crawler104Core:
     def __init__(self):
@@ -15,8 +17,10 @@ class Crawler104Core:
             'Accept-Language':'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4',
             'Connection':'close'
         }
+        self.processedJobId = []
         self.processedJob = []
-        self.sslVerify = True
+        self.totalJobs = 0
+        self.sslVerify = False
     
     # if the element chosen has children
     def __getElementsText(self,elements):
@@ -37,14 +41,87 @@ class Crawler104Core:
         text = text.replace('：','')
         return text
 
+    def getResult(self):
+        return self.processedJob
+    
+    def clearResult(self):
+        self.processedJob = []
+        self.processedJobId = []
+
+    def getTotal(self,url):
+        totalCount = 0
+        totalPage = 0
+
+        retryRequest = requests.Session()
+        retryRequest.mount( 'https://', HTTPAdapter( max_retries = 5 ) )
+        res = retryRequest.get( url.format(1), headers=self.header_info, timeout=5, verify=self.sslVerify)
+        soup = BeautifulSoup(res.text,'html.parser')
+        try:
+            totalCount = int( re.search( r'\"{}\":(\d*)'.format("totalCount"), soup.text).group(1))
+            totalPage = int( re.search( r'\"{}\":(\d*)'.format("totalPage"), soup.text).group(1))
+        except:
+            print("ERROR: getTotal()")
+        return {'totalCount':totalCount,'totalPage':totalPage}
+
+    def start_crawl(self,urls):
+        url_infos = self.url_siever(urls)
+        totalJobs = 0
+        urls = []
+        for url_info in url_infos:
+            totalJobs += url_info[1]
+            urls.append(url_info[0])
+        self.totalJobs = totalJobs
+
+        for url in urls:
+            url_jobs = self.crawl_url(url)
+            self.processedJob += url_jobs
+
+    def crawl_url(self, root_url):
+        totalCount, totalPage = self.getTotal(root_url)['totalCount'],self.getTotal(root_url)['totalPage']
+        currentPage = 1
+        currentCount = 1
+
+        jobList = []
+        try:
+            while(currentPage <= totalPage):
+                print( 'Page: [{}/{}]'.format(currentPage,totalPage))
+                retryRequest = requests.Session()
+                retryRequest.mount( 'https://', HTTPAdapter( max_retries = 5 ) )
+                
+                res = retryRequest.get( root_url.format( currentPage ),headers=self.header_info, timeout=5, verify=self.sslVerify)
+                soup = BeautifulSoup(res.text,'html.parser')
+                
+                jobItems = []
+                jobItems = soup.select('.js-job-item')
+                for job in jobItems:
+                    jobname = job.select_one('.js-job-link').text
+                    joblink = job.select_one('.js-job-link')['href']
+                    joblink = 'https:' + joblink
+
+                    company = job.select_one('.b-list-inline a').text
+                    company = self.__cleanText(company)
+
+                    jobDetail = self.getJobDetail(joblink)
+                    #not tested yet
+                    if jobDetail:
+                        moreJobDetail = {'工作名稱':jobname, '公司':company, '工作連結':joblink}
+                        jobDetail.update(moreJobDetail)
+                        jobList.append(jobDetail)
+                    print( '[{}/{}|{}/{}] {}'.format(currentCount,totalCount, len(self.processedJob), self.totalJobs, joblink))
+                    currentCount += 1
+                currentPage += 1
+        except KeyboardInterrupt:
+            return jobList
+        return jobList
+
     def getJobDetail(self,joblink):
         jobDetailDict = {}
         if (not re.search( r'job/(.*)\?jobsource', joblink ) == None):
             jobno = re.search( r'job/(.*)\?jobsource', joblink ).group(1)
-            if not jobno in self.processedJob:
+            if not jobno in self.processedJobId:
                 jobDetailDict = self.getStandardJobDetail(jobno)
             else:
-                print("{} is proccesed".format(jobno))
+                print("{} is processed".format(jobno))
         else:
             print("joblink invalid : {}".format(joblink))
         return jobDetailDict
@@ -97,146 +174,119 @@ class Crawler104Core:
         jobDetailDict["職務類別"] = jobDetailDict["職務類別"].replace(" ",'').replace("認識「」職務詳細職類分析(工作內容、薪資分布..)更多相關工作","")
         jobDetailDict['上班地點'] = jobDetailDict['上班地點'].replace(" ",'').replace("地圖找工作","")
         
-        self.processedJob.append(jobno)
+        self.processedJobId.append(jobno)
         #print(jobDetailDict)
         return jobDetailDict
 
-    def getTotal(self,url):
-        retryRequest = requests.Session()
-        retryRequest.mount( 'https://', HTTPAdapter( max_retries = 5 ) )
-        res = retryRequest.get( url.format(1),headers=self.header_info, timeout=5, verify=self.sslVerify)
-        soup = BeautifulSoup(res.text,'html.parser')
+    def url_siever(self,urls):
+        url_infos = []
+        for url in urls:
+            totalCount = self.getTotal(url)['totalCount']
+            is_passed = True
+            if totalCount > 3000: 
+                is_passed = False
+            url_infos.append((url, totalCount, is_passed))
+        
+        url_infos = self.sieve_url_with_param(url_infos, 'jobcat')
+        url_infos = [url_info for url_info in url_infos if not url_info[1] == 0 ]
+        url_infos = self.sieve_url_with_param(url_infos, 'area')
+        url_infos = [url_info for url_info in url_infos if not url_info[1] == 0]
+        
+        passed = []
+        unpassed = []
+        for url_info in url_infos:
+            if not url_info[2]:
+                unpassed.append(url_info)
+            else:
+                passed.append(url_info)
+        #print(passed)
+        #print(unpassed)
+        print("passed_url: {}".format(len(passed)))
+        print("unpassed_url: {}".format(len(unpassed)))
 
-        totalCount = int( re.search( r'\"{}\":(\d*)'.format("totalCount"), soup.text).group(1))
-        totalPage = int( re.search( r'\"{}\":(\d*)'.format("totalPage"), soup.text).group(1))
-        return [totalCount,totalPage]
+        return url_infos
 
-    def start_crawl(self, root_url):
-        totalCount, totalPage = self.getTotal(root_url)
-        currentPage = 1
-        currentCount = 1
+    def sieve_url_with_param(self, url_infos, param_name):
+        #url_infos sieved with parameter param_name
+        url_info_list = []
 
-        jobList = []
+        for url, urlCount, urlPass in url_infos:
+            param_list = []
+            baseurl = re.sub(r'(.*)(&{}=\d+)(.*)'.format(param_name),r'\g<1>\g<3>',url)
+
+            if urlPass:
+                url_info_list.append((url, urlCount, urlPass))
+            else:
+                if param_name in url:
+                    param_val = re.search(r'&{}=(\d+)'.format(param_name),url).group(1)
+                    self.traverse_param_tree(param_list, baseurl, param_name, param_val)
+                else:
+                    self.traverse_param_tree(param_list, url, param_name)
+
+                for param in param_list:
+                    param_url = baseurl + "&{}=".format(param_name) + param[0]
+                    url_info_list.append((param_url, param[1], param[2]))
+        
+        return url_info_list
+
+    def traverse_param_tree(self,param_list, base_url, param_name, param_val = None):
+        url = base_url + "&{}=".format(param_name)
+        if not param_val:
+            for param in self.param_child(param_name):
+                self.traverse_param_tree(param_list, base_url, param_name, param)
+        else:
+            totalCount = self.getTotal(url + param_val)['totalCount']
+            if totalCount <= 3000:
+                param_list.append((param_val, totalCount, True))
+
+            elif totalCount > 3000 and len(self.param_child(param_name, param_val)) == 0:
+                param_list.append((param_val, totalCount, False))
+
+            else:
+                for param in self.param_child(param_name, param_val):
+                    self.traverse_param_tree(param_list, base_url, param_name, param)
+
+    def param_child(self,param_name, param_val = None):
+        param_children_value = []
+        xml_path = os.path.join(settings.configDirectory,'104_config.xml')
+        tree = xml_etree_ElementTree.parse(xml_path)
         try:
-            while(currentPage <= totalPage):
-                retryRequest = requests.Session()
-                retryRequest.mount( 'https://', HTTPAdapter( max_retries = 5 ) )
-                
-                res = retryRequest.get( root_url.format( currentPage ),headers=self.header_info, timeout=5, verify=self.sslVerify)
-                soup = BeautifulSoup(res.text,'html.parser')
-                print( 'Page: [{}/{}]'.format(currentPage,totalPage))
-                
-                jobItems = []
-                jobItems = soup.select('.js-job-item')
-                for job in jobItems:
-                    jobname = job.select_one('.js-job-link').text
-                    joblink = job.select_one('.js-job-link')['href']
-                    joblink = 'https:' + joblink
+            parent = tree.find('{}'.format(param_name))
+            if param_val:
+                parent = parent.find('.//*[@value="{}"]'.format(param_val))
+            for child in parent:
+                param_children_value.append(child.attrib['value'])
+        except TypeError:
+            print("{} param_name {} does not exist".format(TypeError, param_name))
+            raise
+        return param_children_value
 
-                    company = job.select_one('.b-list-inline a').text
-                    company = self.__cleanText(company)
-
-                    jobDetail = self.getJobDetail(joblink)
-                    #not tested yet
-                    if jobDetail:
-                        moreJobDetail = {'工作名稱':jobname, '公司':company, '工作連結':joblink}
-                        jobDetail.update(moreJobDetail)
-                        jobList.append(jobDetail)
-                    print( '[{}/{}] {}'.format(currentCount,totalCount,joblink))
-                    currentCount += 1
-                currentPage += 1
-        except KeyboardInterrupt:
-            return jobList
-        return jobList
-
-def url_param_dict(url):
-    params = re.findall(r"[^&?]*?=[^&?]*",url)
-    param_dict = {}
-    for param in params:
-        p_name,p_val = param.split("=")
-        param_dict[p_name] = p_val.split("%2C")
-    return param_dict 
-
-def url_siever(urls):
-    url_infos = [(url, getTotal(url), False) if getTotal(url) > 3000 else (url, getTotal(url), True) for url in urls]
-    url_infos = sieve_url_with_param(url_infos, 'area')
-    url_infos = [url_info for url_info in url_infos if not url_info[1] == 0 ]
-    url_infos = sieve_url_with_param(url_infos, 'jobcat')
-    url_infos = [url_info for url_info in url_infos if not url_info[1] == 0 ]
-    print(url_infos)
-    print(len(url_infos))
-    for url_info in url_infos:
-        if not url_info[2]:
-            print(url_info)
-    print("End")
-
-def sieve_url_with_param(url_infos, param_name, param_val=None):
-    url_list = []
-    param_list = []
-
-    for url, urlCount, urlPass in url_infos:
-        if not urlPass:
-            traverse_param_tree(param_list, url, param_name, param_val)
-        for param in param_list:
-            param_url = url + "&{}=".format(param_name) + param[0]
-            url_list.append((param_url, param[1], param[2]))
-    
-    return url_list
-
-def traverse_param_tree(param_list, base_url, param_name, param_val = None):
-    url = base_url + "&{}=".format(param_name)
-    if not param_val:
-        for param in param_child(param_name):
-            traverse_param_tree(param_list, base_url, param_name, param)
-    elif getTotal(url + param_val) <= 3000:
-        param_list.append((param_val, getTotal(url + param_val), True))
-
-    elif getTotal(url + param_val) > 3000 and len(param_child(param_name, param_val)) == 0:
-        param_list.append((param_val, getTotal(url + param_val), False))
-
-    else:
-        for param in param_child(param_name, param_val):
-            traverse_param_tree(param_list, base_url, param_name, param)
-
-def param_child(param_name, param_val = None):
-    param_children_value = []
-    xml_path = os.path.join(settings.configDirectory,'104_config.xml')
-    tree = xml_etree_ElementTree.parse(xml_path)
-    try:
-        parent = tree.find('{}'.format(param_name))
-        if param_val:
-            parent = parent.find('.//*[@value="{}"]'.format(param_val))
-        for child in parent:
-            param_children_value.append(child.attrib['value'])
-    except TypeError:
-        print("{} param_name {} does not exist".format(TypeError, param_name))
-        raise
-    return param_children_value
-
-def getTotal(url):
-    totalCount = 0
-    totalPage = 0
-
-    header_info = {
-        'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/70.0.3538.110 Safari/537.36',
-        'Accept-Language':'zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4',
-        'Connection':'close'
-    }
-    retryRequest = requests.Session()
-    retryRequest.mount( 'https://', HTTPAdapter( max_retries = 5 ) )
-    res = retryRequest.get( url.format(1),headers=header_info, timeout=5, verify=True)
-    soup = BeautifulSoup(res.text,'html.parser')
-    try:
-        totalCount = int( re.search( r'\"{}\":(\d*)'.format("totalCount"), soup.text).group(1))
-        totalPage = int( re.search( r'\"{}\":(\d*)'.format("totalPage"), soup.text).group(1))
-    except:
-        totalCount = 0
-        totalPage = 0
-    return totalCount
-
+        
 
 if __name__ == "__main__":
-    url = 'https://www.104.com.tw/jobs/search/?jobsource=2018indexpoc&page=1'
-    url2 = 'https://www.104.com.tw/jobs/search/?jobsource=2018indexpoc&page=1&jobcat=2003000000&area=6001002000'
-    url_siever([url])
-    #print(url_list)
+    url1 = 'https://www.104.com.tw/jobs/search/?jobsource=2018indexpoc&page={}&indcat=1004002000'
+    url2 = 'https://www.104.com.tw/jobs/search/?jobsource=2018indexpoc&page={}&jobcat=2007002000'
+    url3= 'https://www.104.com.tw/jobs/search/?jobsource=2018indexpoc&page={}&jobcat=2017000000'
+    urls = [url3]
+    cc = Crawler104Core()
+    cc.start_crawl(urls)
+    df = pandas.DataFrame(cc.getResult())
+    df.to_excel('test.xlsx')
+
+    #traverse_param_tree tests
+    """
+    param_list = []
+    cc.traverse_param_tree(param_list, url3, 'jobcat', '2017000000')
+    print(param_list)
+    """
+    #sieve_url_with_param tests
+    """
+    for url in urls:
+        url_infos = [(url, cc.getTotal(url)['totalCount'], False)]
+        url_info_list = cc.sieve_url_with_param(url_infos,'jobcat')
+        print(url_info_list)
+        print(len(url_info_list))
+    """
+
+    
+    
